6dccc4774eef3b9094a74ab90be1f70b2fa438cf
marushimellow <57819442+marushimellow@users.noreply.github.com>
2020-01-18 12:51:12 +0800

    Create ajax.js

---
+++ ajax.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+ $("#btn-table").click(function() {
+  $("#container").load("table.html");
+  });
+});
+$(document).ready(function() {
+ $("#btn-dialog").click(function() {
+  $("#container").load("dialog.html");
+  });
+});
+
+
